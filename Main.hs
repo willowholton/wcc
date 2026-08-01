c3c5e452179ed016429b48236e5be38cfbaf412e
willowholton <willow.holton@gmail.com>
2026-08-01 09:55:41 -0700

    rewrote run functions for readability

--- Main.hs
+++ Main.hs
@@ -4,50 +4,68 @@ import System.Exit (exitSuccess, exitFailure)
 import System.Environment (getArgs)
 import System.Directory (doesFileExist)
 import System.FilePath (dropExtension)
+import System.Process (callProcess)
 import Lexer
 import Parser
 import Asm
+import Distribution.Simple.Command (ShowOrParseArgs(ParseArgs))
+import System.Posix (fileAccess)
+import Foreign.C (errnoToIOError)
 
 main :: IO ()
 main = do
   args <- getArgs
-  run args
+  case args of
+    [file]       -> run Nothing file
+    [flag, file] -> run (Just flag) file
+    _            -> exitFailure
 
 
-run :: [String] -> IO ()
-run [flag, file] = do
-  let outFile = dropExtension file ++ ".s"
+run :: Maybe String -> FilePath -> IO ()
+run flag file = do
   exists <- doesFileExist file
   if not exists
     then do
-      putStrLn("Error: " ++ file ++ " does not exist")
+      putStrLn ("Error - file " ++ file ++ " does not exist")
       exitFailure
-    else do
-      contents <- readFile file
-      case runCompiler flag contents of
-        Left err -> do
-          putStrLn err
-          exitFailure
-        Right NoOutput       -> exitSuccess
-        Right (PrintAst program) -> do
-          putStrLn (printProgram program)
-          exitSuccess
-        Right (WriteAsm asmString) -> do
-          writeFile outFile asmString
-          exitSuccess
-run _ = exitFailure
+    else case (getStage flag) of
+      Left err -> do
+        putStrLn err
+        exitFailure
+      Right stage -> do
+        contents <- readFile file
+        case runCompiler stage contents of
+          Left err -> do
+            putStrLn err
+            exitFailure
+          Right NoOutput -> exitSuccess
+          Right (PrintAst program) -> do
+            putStrLn (printProgram program)
+            exitSuccess
+          Right (WriteAsm asmProgram) -> do
+            let outFile = dropExtension file ++ ".s"
+            writeFile outFile asmProgram
+            if stage == AsmStage
+              then exitSuccess
+              else do
+                callProcess "gcc" [outFile, "-o", dropExtension file]
+                exitSuccess
 
-runCompiler :: String -> (String -> (Either String Result))
-runCompiler flag contents = do
+
+runCompiler :: Stage -> (String -> (Either String Result))
+runCompiler stage contents = do
   tokens  <- lexer contents
-  program <- parseProgram tokens
-  let asmProgram = getProgram program
-  case flag of
-    "--lex"     -> Right NoOutput
-    "--parse"   -> Right (PrintAst program)
-    "--codegen" -> Right NoOutput
-    "-S"        -> Right (WriteAsm (printAsmProgram asmProgram))
-    _           -> Left "Not implemented yet"
+  if stage == LexStage
+    then Right NoOutput
+    else do
+      program <- parseProgram tokens
+      if stage == ParseStage
+        then Right (PrintAst program)
+        else do
+          let asmProgram = getProgram program
+          if stage == CodegenStage
+            then Right NoOutput
+            else Right (WriteAsm (printAsmProgram asmProgram))
 
 data Result
   = NoOutput
@@ -55,6 +73,25 @@ data Result
   | WriteAsm String
   deriving (Show, Eq)
 
+-- stages used to mark where the process can end:
+data Stage
+  = FullStage
+  | LexStage
+  | ParseStage
+  | CodegenStage
+  | AsmStage
+  deriving (Show, Eq)
+
+-- take a flag (if given) and determine what stage of the process to stop at:
+getStage :: Maybe String -> Either String Stage
+getStage Nothing            = Right FullStage
+getStage (Just "--lex")     = Right LexStage
+getStage (Just "--parse")   = Right ParseStage
+getStage (Just "--codegen") = Right CodegenStage
+getStage (Just "-S")        = Right AsmStage
+getStage (Just unknown)     = Left ("Error invalid flag: " ++ unknown)
+
+-- determine whether the given flag is a valid one or not:
 validFlag :: String -> Bool
 validFlag flag = case flag of
   "--lex"     -> True
